09b70fc3f47489c3891ebbaad238ac959c0cb659
Shay Symonette <ssymonette@myseneca.ca>
2025-11-15 18:29:55 -0500

    Added my functions

--- RealEstate.c
+++ RealEstate.c
@@ -13,12 +13,17 @@
 //	30-Oct-25	D. Gonzales		Started PrintReport function
 //	06-Nov-25	D. Gonzales		Finished the PrintReport function
 //  11-Nov-25   E. Karaman      Added function DeterminePerformance
+//  15-Nov-25   S. Symonette    Added CalculateROI, CalculateCurrentValue, CalculateCapitalGains functions
+//  15-Nov-25   S. Symonette    Fixed determinePerformance function to fit new function definitions
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#include "Property.h"
 #include <stdio.h>
 #include <string.h>
 #include "RealEstate.h"
+
+#define INTEREST_RATE 0.05
 
 int ExtractDataFromFile(struct RealEstateCompany* realEstate, char apartmentFilename[],
 	char townhouseFilename[], char semiDetachedHouseFilename[]) {
@@ -28,7 +33,7 @@ int ExtractDataFromFile(struct RealEstateCompany* realEstate, char apartmentFile
 	FILE* fpApartment = fopen(apartmentFilename, "r");
 	if (fpApartment != NULL) {
 		for (int i = 0; i < NUM_APARTMENTS && retVal > 0; ++i) {
-			retVal = fscanf(fpApartment, " %[^\n]s ", realEstate->apartment.property[i].address);
+			retVal = fscanf(fpApartment, " %[^\n] ", realEstate->apartment.property[i].address);
 			retVal = fscanf(fpApartment, "%d", &realEstate->apartment.property[i].numRooms);
 			retVal = fscanf(fpApartment, "%d", &realEstate->apartment.property[i].numBathrooms);
 			retVal = fscanf(fpApartment, "%lf", &realEstate->apartment.property[i].purchasePrice);
@@ -44,7 +49,7 @@ int ExtractDataFromFile(struct RealEstateCompany* realEstate, char apartmentFile
 	FILE* fpTownhouse = fopen(townhouseFilename, "r");
 	if (fpApartment != NULL) {
 		for (int i = 0; i < NUM_TOWNHOUSES && retVal > 0; ++i) {
-			retVal = fscanf(fpTownhouse, " %[^\n]s ", realEstate->townhouse.property[i].address);
+			retVal = fscanf(fpTownhouse, " %[^\n] ", realEstate->townhouse.property[i].address);
 			retVal = fscanf(fpTownhouse, "%d", &realEstate->townhouse.property[i].numRooms);
 			retVal = fscanf(fpTownhouse, "%d", &realEstate->townhouse.property[i].numBathrooms);
 			retVal = fscanf(fpTownhouse, "%lf", &realEstate->townhouse.property[i].purchasePrice);
@@ -63,7 +68,7 @@ int ExtractDataFromFile(struct RealEstateCompany* realEstate, char apartmentFile
 	FILE* fpSemiDetachedHouse = fopen(semiDetachedHouseFilename, "r");
 	if (fpApartment != NULL) {
 		for (int i = 0; i < NUM_SEMIDETACHED_HOUSES && retVal > 0; ++i) {
-			retVal = fscanf(fpSemiDetachedHouse, " %[^\n]s ", realEstate->semiDetachedHouse.property[i].address);
+			retVal = fscanf(fpSemiDetachedHouse, " %[^\n] ", realEstate->semiDetachedHouse.property[i].address);
 			retVal = fscanf(fpSemiDetachedHouse, "%d", &realEstate->semiDetachedHouse.property[i].numRooms);
 			retVal = fscanf(fpSemiDetachedHouse, "%d", &realEstate->semiDetachedHouse.property[i].numBathrooms);
 			retVal = fscanf(fpSemiDetachedHouse, "%lf", &realEstate->semiDetachedHouse.property[i].purchasePrice);
@@ -89,6 +94,64 @@ void PrintReport(struct RealEstateCompany realEstate) {
 		realEstate.semiDetachedHouse.monthlyEarnings, realEstate.semiDetachedHouse.annualROI, realEstate.semiDetachedHouse.currentValue, realEstate.semiDetachedHouse.capitalGains);
 }
 
+double CalculateROI(struct Property* properties, int numProperties, double (*monthlyEarningsFunc)(struct Property*)) {
+	if (properties == NULL || numProperties <= 0 || monthlyEarningsFunc == NULL)
+		return 0.0;
+
+	double totalROI = 0.0;
+
+	for (int i = 0; i < numProperties; i++) {
+		double monthlyEarnings = monthlyEarningsFunc(&properties[i]);
+		double purchasePrice = properties[i].purchasePrice;
+
+		if (purchasePrice > 0.0)
+			totalROI += 100.0 * 12.0 * monthlyEarnings / purchasePrice;
+	}
+
+	return totalROI;
+}
+
+double CalculateCurrentValue(struct Property* properties, int numProperties, double (*monthlyEarningsFunc)(struct Property*)) {
+	if (properties == NULL || numProperties <= 0 || monthlyEarningsFunc == NULL)
+		return 0.0;
+
+	double totalValue = 0.0;
+
+	for (int i = 0; i < numProperties; i++) {
+		double monthlyEarnings = monthlyEarningsFunc(&properties[i]);
+		totalValue += (12.0 * monthlyEarnings) / INTEREST_RATE;
+	}
+
+	return totalValue;
+}
+
+double CalculateCapitalGains(struct Property* properties, int numProperties, double (*monthlyEarningsFunc)(struct Property*)) {
+	if (properties == NULL || numProperties <= 0 || monthlyEarningsFunc == NULL)
+		return 0.0;
+
+	double totalGain = 0.0;
+
+	for (int i = 0; i < numProperties; i++) {
+		double purchasePrice = properties[i].purchasePrice;
+		double currentValue = (12.0 * monthlyEarningsFunc(&properties[i])) / INTEREST_RATE;
+		totalGain += (currentValue - purchasePrice);
+	}
+
+	return totalGain;
+}
+
+double ApartmentMonthlyEarnings(struct Property* p) {
+	return p->monthlyRent - p->monthlyCondoFees;
+}
+
+double TownhouseMonthlyEarnings(struct Property* p) {
+	return p->monthlyRent - p->monthlyUtilities - p->monthlyCondoFees - p->monthlyPropertyTax;
+}
+
+double SemiDetachedMonthlyEarnings(struct Property* p) {
+	return p->monthlyRent - p->monthlyUtilities - p->monthlyPropertyTax;
+}
+
 void DeterminePerformance(struct RealEstateCompany* realEstate) {
 	// Apartments
 	realEstate->apartment.monthlyEarnings = 0;
@@ -97,9 +160,9 @@ void DeterminePerformance(struct RealEstateCompany* realEstate) {
 			realEstate->apartment.property[i].monthlyRent -
 			realEstate->apartment.property[i].monthlyCondoFees;
 	}
-	realEstate->apartment.annualROI = CalculateROI(&realEstate->apartment, NUM_APARTMENTS);
-	realEstate->apartment.currentValue = CalculateCurrentValue(&realEstate->apartment, NUM_APARTMENTS);
-	realEstate->apartment.capitalGains = CalculateCapitalGains(&realEstate->apartment, NUM_APARTMENTS);
+	realEstate->apartment.annualROI = CalculateROI(realEstate->apartment.property, NUM_APARTMENTS, ApartmentMonthlyEarnings);
+	realEstate->apartment.currentValue = CalculateCurrentValue(realEstate->apartment.property, NUM_APARTMENTS, ApartmentMonthlyEarnings);
+	realEstate->apartment.capitalGains = CalculateCapitalGains(&realEstate->apartment.property, NUM_APARTMENTS, ApartmentMonthlyEarnings);
 
 	// Townhouses
 	realEstate->townhouse.monthlyEarnings = 0;
@@ -110,9 +173,9 @@ void DeterminePerformance(struct RealEstateCompany* realEstate) {
 			realEstate->townhouse.property[i].monthlyUtilities -
 			realEstate->townhouse.property[i].monthlyPropertyTax;
 	}
-	realEstate->townhouse.annualROI = CalculateROI(&realEstate->townhouse, NUM_TOWNHOUSES);
-	realEstate->townhouse.currentValue = CalculateCurrentValue(&realEstate->townhouse, NUM_TOWNHOUSES);
-	realEstate->townhouse.capitalGains = CalculateCapitalGains(&realEstate->townhouse, NUM_TOWNHOUSES);
+	realEstate->townhouse.annualROI = CalculateROI(realEstate->townhouse.property, NUM_TOWNHOUSES,TownhouseMonthlyEarnings);
+	realEstate->townhouse.currentValue = CalculateCurrentValue(realEstate->townhouse.property, NUM_TOWNHOUSES, TownhouseMonthlyEarnings);
+	realEstate->townhouse.capitalGains = CalculateCapitalGains(realEstate->townhouse.property, NUM_TOWNHOUSES, TownhouseMonthlyEarnings);
 
 	// Semi-Detached Houses
 	realEstate->semiDetachedHouse.monthlyEarnings = 0;
@@ -122,7 +185,7 @@ void DeterminePerformance(struct RealEstateCompany* realEstate) {
 			realEstate->semiDetachedHouse.property[i].monthlyUtilities -
 			realEstate->semiDetachedHouse.property[i].monthlyPropertyTax;
 	}
-	realEstate->semiDetachedHouse.annualROI = CalculateROI(&realEstate->semiDetachedHouse, NUM_SEMIDETACHED_HOUSES);
-	realEstate->semiDetachedHouse.currentValue = CalculateCurrentValue(&realEstate->semiDetachedHouse, NUM_SEMIDETACHED_HOUSES);
-	realEstate->semiDetachedHouse.capitalGains = CalculateCapitalGains(&realEstate->semiDetachedHouse, NUM_SEMIDETACHED_HOUSES);
+	realEstate->semiDetachedHouse.annualROI = CalculateROI(realEstate->semiDetachedHouse.property, NUM_SEMIDETACHED_HOUSES, SemiDetachedMonthlyEarnings );
+	realEstate->semiDetachedHouse.currentValue = CalculateCurrentValue(realEstate->semiDetachedHouse.property, NUM_SEMIDETACHED_HOUSES, SemiDetachedMonthlyEarnings);
+	realEstate->semiDetachedHouse.capitalGains = CalculateCapitalGains(realEstate->semiDetachedHouse.property, NUM_SEMIDETACHED_HOUSES, SemiDetachedMonthlyEarnings);
 }
